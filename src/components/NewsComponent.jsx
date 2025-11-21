6e4f40fd4eb94e69b6e768a5b84f9d9c79f05a99
Raxon Suri <raxonsuriperez@gmail.com>
2025-11-20 23:17:38 -0500

    API Added

--- src/components/NewsComponent.jsx
+++ src/components/NewsComponent.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useEffect } from 'react'
+
+const API_KEY= import.meta.env.VITE_API_KEY;
 
 export const NewsComponent = () => {
+  const fetchData = async() => {
+    const request = await fetch(`https://newsapi.org/v2/everything?q=Tech&from=2025/11/17&sortBy=popularity&apiKey=${API_KEY}`);
+    const data = await request.json();
+
+    console.log(data)
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
   return (
     <>
-        hello
+        <h1 className='bg-linear-to-b from-red-400 to-red-500'>Top 10 Tech News of Today</h1>
+        <ul>
+          <li></li>
+        </ul>
     </>
   )
 }
